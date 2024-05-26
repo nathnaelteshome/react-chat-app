5404e824470c173fdceb366802906cfbbcf60adc
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-05-26 16:47:15 +0300

    chore: Add emoji-picker-react package and update container styles

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+  console.log(text);
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Dolor omnis quos cum doloribus.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              ullam ratione minus animi, sit officia incidunt veritatis iusto
+              facilis aut fugit dolore, sint culpa ab, possimus consequatur
+              temporibus voluptatum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              ullam ratione minus animi, sit officia incidunt veritatis iusto
+              facilis aut fugit dolore, sint culpa ab, possimus consequatur
+              temporibus voluptatum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              ullam ratione minus animi, sit officia incidunt veritatis iusto
+              facilis aut fugit dolore, sint culpa ab, possimus consequatur
+              temporibus voluptatum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://images.pexels.com/photos/325173/pexels-photo-325173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              ullam ratione minus animi, sit officia incidunt veritatis iusto
+              facilis aut fugit dolore, sint culpa ab, possimus consequatur
+              temporibus voluptatum cupiditate!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img src="./emoji.png" onClick={() => setOpen(!open)} alt="" />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./chatList.css";
+const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="/search.png" alt="" />
+          <input type="text" placeholder="Search" />
+        </div>
+        <img
+          className="addIcon"
+          onClick={() => setAddMode(!addMode)}
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello there missy</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello there missy</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello there missy</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello there missy</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatList;
